753d6c604c15c6268a6ee7ed168910e7d256e1c9
mytestkube <mednyi.vsadnik@gmail.com>
2020-09-01 22:02:47 +0300

    'components'

--- todo/todo_class.js
+++ todo/todo_class.js
@@ -1,7 +1,79 @@
 'use strict';
 
 const todos = [{title: 'todo1', status: true}, {title: 'todo2', status: false}];
-class TodoView {
+class Component {
+    constructor(data = {}) {
+        this.data = data;
+        this.methods = this.methods();
+        Object.keys(this.methods).forEach(methodName => {
+            this.methods[methodName]  = this.methods[methodName].bind(this)
+        })
+    }
+    methods () {return {}}
+    template () {return `<div></div>`}
+    destroy () {
+        this.$el.remove();
+        delete this;
+    }
+    onRender () {}
+    render () {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.template();
+        if (this.$el) {
+            this.$el.innerHTML = wrapper.firstElementChild.innerHTML;
+        } else {
+            this.$el = wrapper.firstElementChild;
+        }
+        this.onRender();
+        return this.$el
+    }
+}
+
+class TodoView extends Component {
+    constructor(data = { title: '', status: false }) {
+        super(data);
+    }
+    methods () {
+        return {
+            remove() {
+                this.destroy()
+            },
+            changeTitle() {
+                const titleInputValue = this.$el.children[0].value;
+                this.data.title = titleInputValue;
+                this.render();
+            },
+            changeStatus() {
+                const inputStatus = +this.$el.children[1].value;
+                this.data.status = inputStatus;
+                this.render();
+            }
+        }
+    }
+    template () {
+        return `
+                <div class="todo">
+                  <input class ="input" value="${this.data.title}" id="myTextField" placeholder="Добавить задачу">
+                  <select class ="dropdown">
+                     <option ${this.data.status ? 'selected' : ''} value=1>Сделано</option>
+                     <option ${!this.data.status ? 'selected' : ''} value=0>Не сделано</option>
+                  </select>
+                  <span class="icon" data-action="delete">X</span>
+                </div> 
+        `
+    }
+    onRender () {
+        const deleteButton = this.$el.children[2];
+        deleteButton.addEventListener('click', this.methods.remove);
+        const titleInput = this.$el.children[0];
+        titleInput.addEventListener('blur', this.methods.changeTitle);
+        const inputStatus = this.$el.children[1];
+        inputStatus.addEventListener('change', this.methods.changeStatus);
+        return this.$el;
+    }
+}
+
+/*class TodoView {
     constructor(data = {title: '', status: false}) {
         this.data = data;
         Object.keys(this.methods).forEach(methodName => {
@@ -9,11 +81,11 @@ class TodoView {
         })
     }
     template () {
-        return ` 
+        return `
                  <input class ="input" value="${this.data.title}" id="myTextField" placeholder="Добавить задачу">
                  <select class ="dropdown">
-                     <option ${this.data.status ? 'selected' : ''}>Сделано</option>
-                     <option ${!this.data.status ? 'selected' : ''}>Не сделано</option>
+                     <option ${this.data.status ? 'selected' : ''} value=1>Сделано</option>
+                     <option ${!this.data.status ? 'selected' : ''} value=0>Не сделано</option>
                  </select>
                  <span class="icon" data-action="delete">X</span>
         `
@@ -27,6 +99,11 @@ class TodoView {
             const titleInputValue = this.$el.children[0].value;
             this.data.title = titleInputValue;
             this.render();
+        },
+        changeStatus () {
+            const inputStatus = + this.$el.children[1].value;
+            this.data.status = inputStatus;
+            this.render();
         }
     }
     render () {
@@ -39,9 +116,11 @@ class TodoView {
         deleteButton.addEventListener('click', this.methods.remove);
         const titleInput = this.$el.children[0];
         titleInput.addEventListener('blur', this.methods.changeTitle);
+        const inputStatus = this.$el.children[1];
+        inputStatus.addEventListener('change', this.methods.changeStatus);
         return this.$el;
     }
-}
+}*/
 
 class TodoList {
     constructor(data = {filter: '', tasks: []}) {
